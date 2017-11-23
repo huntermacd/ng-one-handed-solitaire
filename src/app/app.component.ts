e6f42d2259122ddbb01429f66b1800d69b1d81a7
Hunter MacDermut <whosebluesanyway@gmail.com>
2017-11-22 22:24:36 -0700

    Implement shuffle and initial deal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { AppService } from './app.service';
 
 /*
@@ -30,11 +29,33 @@ methods:
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  deck$: Observable<any>;
+  deck = [];
+  tableau = [];
 
   constructor(private appService: AppService) { }
 
   ngOnInit() {
-    this.deck$ = this.appService.loadDeck();
+    this.appService.loadDeck()
+      .subscribe(deck => this.deck = deck);
+  }
+
+  shuffle(deck) {
+    let m = deck.length, t, i;
+
+      while (m) {
+        i = Math.floor(Math.random() * m--);
+
+        t = deck[m];
+        deck[m] = deck[i];
+        deck[i] = t;
+      }
+
+      return deck;
+  }
+
+  newGame() {
+    this.deck = this.shuffle(this.deck);
+    this.tableau = this.deck.slice(0, 4);
+    this.deck = this.deck.slice(4);
   }
 }
