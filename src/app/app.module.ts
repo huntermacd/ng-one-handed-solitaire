de419e5c2fe0dd82213c53ab48dab25b2204e2c1
Hunter MacDermut <whosebluesanyway@gmail.com>
2017-11-24 17:06:56 -0700

    Add Material, style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppService } from './app.service';
 
@@ -15,7 +18,9 @@ import { CardComponent } from './card/card.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
